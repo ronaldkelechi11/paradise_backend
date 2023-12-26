7ac3148d5db4babdbf97474442d9f0520d3d7762
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-12-26 09:09:58 +0100

    user balance plus 2 every hour after deposit

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { mongoose } = require('mongoose')
 const app = express()
 
 const PORT = process.env.PORT || 4000
-const dbUrl = "mongodb+srv://ronaldkelechi11:wKit0n89fCFCtTU5@paradiseinvestment.msdxawb.mongodb.net/?retryWrites=true&w=majority" || process.env.MONGO_URL
-// const dbUrl = "mongodb://127.0.0.1:27017/paradiseDB"
+// const dbUrl = "mongodb+srv://ronaldkelechi11:wKit0n89fCFCtTU5@paradiseinvestment.msdxawb.mongodb.net/?retryWrites=true&w=majority" || process.env.MONGO_URL
+const dbUrl = "mongodb://127.0.0.1:27017/paradiseDB"
 
 
 
--- routes/Dashboard.js
+++ routes/Dashboard.js
@@ -42,6 +42,7 @@ router.post("/:username/deposit",
     upload.single('file'),
     async (req, res) => {
         var username = req.params.username
+        var oneHour = 60 * 60 * 1000
 
         const transaction = new Transaction({
             amount: req.body.amount,
@@ -66,6 +67,7 @@ router.post("/:username/deposit",
                     user.updateOne({ transactions: user.transactions })
                         .then((result) => {
                             res.status(200).send()
+                            updateProfitAuto()
                         }).catch((err) => {
                             console.log(err);
                         });
@@ -73,6 +75,21 @@ router.post("/:username/deposit",
             }).catch((err) => {
                 console.log(err);
             });
+
+
+        function updateProfitAuto() {
+            setInterval(async () => {
+                const user = await User.findOne({ username: username })
+                if (user) {
+                    user.updateOne({ profit: parseFloat(user.profit + 2) })
+                        .then((result) => {
+                            console.log("updated profit");
+                        }).catch((err) => {
+                            console.log('cant update profit');
+                        });
+                }
+            }, oneHour);
+        }
     })
 
 
@@ -80,6 +97,8 @@ router.post("/:username/deposit",
 // Making a withdrawal
 router.post('/:username/withdrawal', async (req, res) => {
     var username = req.params.username
+
+    // Make a code to check balance if it's less than 5 or less than user account balance
 
     var walletAddress = req.body.walletAddress
     var amount = req.body.amount
